c18756bcb504429db3462136018a7a78470d6afc
MNEIMOH <juniornkiangmatiah>
2021-08-04 17:05:11 +0100

    Login and Logout done

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,6 +1,14 @@
+<?php
+  use App\Http\Controllers\ProductController;
+  $total = 0;
+  if (Session::has('user')) {
+    $total = ProductController::cartItem();
+  }
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="/">E-Comm</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -19,7 +27,20 @@
       
     </div>
     <ul class="nav navbar-nav navbar-right">
-      	<li><a class="nav-link" href="#">Cart(0)</a></li>
+      	<li><a class="nav-link" href="#">Cart({{$total}})</a></li>
+        @if(Session::has('user'))
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{Session::get('user')['name']}}
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/logout">Logout</a></li>
+            <li><hr class="dropdown-divider"></li>
+          </ul>
+        </li>
+        @else
+        <li><a href="login">Login</a></li>
+        @endif
     </ul>
   </div>
 </nav>
